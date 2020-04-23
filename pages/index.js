c44cfcd78a8a58f23ab38ff659008711b8401c4c
danilocha <danilocha1@gmail.com>
2020-04-22 21:27:12 -0500

    agregando materialize, layout

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,6 @@
-import Head from "next/head";
 import styled from "styled-components";
-
-const Header = styled.h2`
-  background: blue;
-  text-align: center;
-  width: 100%;
-  color: white;
-`;
+import Layout from "../components/layout/Layout";
 
 export default function Home() {
-  return (
-    <Header>
-      <h2>Hola mundo</h2>
-    </Header>
-  );
+  return <Layout pageTitle="e-duquemos"></Layout>;
 }

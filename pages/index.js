c4d6a90e14e5b429b802b1710d19e34f593c92f0
danilocha <danilocha1@gmail.com>
2020-06-14 19:39:42 -0500

    correccion de mayusculas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import { useRouter } from 'next/router'
 import { AuthContext } from '../context'
 
